7033550a01e7288f96fb64703bd60a2f4471d940
rstorf25 <61019008+rstorf25@users.noreply.github.com>
2024-01-30 13:59:02 -0600

    add mainPage, install react-charts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import MainPage from "./pages/mainPage";
 
 function App() {
   return (
     <div className="App">
-    <h1>Header placeholder</h1>
+      <MainPage/>
+      <h1>Header placeholder</h1>
     </div>
   );
 }
---
+++ src/pages/mainPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function MainPage() {
+  return (
+    <div className="main-page">
+      <h1>Main Page placeholder</h1>
+    </div>
+  );
+}
